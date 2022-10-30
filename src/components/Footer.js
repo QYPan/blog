7cae7c0a1e1399af85bbac5aa60c437b1470c803
PQY <qypan1994@163.com>
2022-10-30 23:49:42 +0800

    refine header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="px-6 py-6 border-t border-gray-200">
+    <footer className="px-4 py-4 lg:px-16 lg:py-6 border-t border-gray-200">
       <p>{`Copyright © 2022 - ${new Date().getFullYear()} 最爱午后红茶`}</p>
     </footer>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,29 +2,132 @@ export function Header() {
   return (
     <div className="">
       <header className="flex flex-row justify-end border-b-2 border-gray-200">
-        <div className="px-5 py-5 lg:px-20 flex flex-row mr-auto">
+        <div className="px-4 lg:px-16 flex flex-row mr-auto">
           <img
-            className="w-24 h-24"
+            className="w-16 h-16 py-0 lg:w-24 lg:h-24"
             src="/assert/images/logo.png"
             alt="first picture"
           />
-          <h1 className="px-5 py-7 text-4xl">
+          <h1 className="px-5 py-7 text-xl lg:text-3xl">
             最爱午后<span className="text-red-400">红茶</span>
           </h1>
         </div>
-        <div className="mt-14 mr-14 mt- text-xl flex gap-6">
-          <a className="" href="#">
-            知识体系
-          </a>
-          <a className="" href="#">
-            阅读
-          </a>
-          <a className="" href="https://blog.csdn.net/u013351484?type=blog">
-            早期博客
-          </a>
-          <a className="" href="#">
-            更多
-          </a>
+        <div className="px-4 lg:px-16 lg:text-xl flex gap-8">
+          <div className="relative group">
+            <button className="py-8 group-hover:text-red-500">知识体系</button>
+            <div className="absolute hidden group-hover:block z-10 w-96 -right-36 gap-4">
+              <div className="p-2">
+                <div className="grid grid-cols-3 border-b-2 bg-gray-200">
+                  <a
+                    href="#"
+                    className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  >
+                    UI框架
+                  </a>
+                  <a
+                    href="#"
+                    className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  >
+                    系统平台
+                  </a>
+                  <a
+                    href="#"
+                    className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  >
+                    编程语言
+                  </a>
+                  <a
+                    href="#"
+                    className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  >
+                    工具
+                  </a>
+                  <a
+                    href="#"
+                    className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  >
+                    算法
+                  </a>
+                  <a
+                    href="#"
+                    className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  >
+                    跨学科
+                  </a>
+                  <a
+                    href="#"
+                    className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  >
+                    游戏
+                  </a>
+                  <a
+                    href="#"
+                    className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  >
+                    英语
+                  </a>
+                  <a
+                    href="#"
+                    className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  >
+                    音乐
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative group">
+            <button className="py-8 group-hover:text-red-500">阅读</button>
+            <div className="absolute hidden group-hover:block z-10 w-48 -right-20 gap-4">
+              <div className="grid grid-cols-2 p-2">
+                <a
+                  href="#"
+                  className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                >
+                  读书
+                </a>
+                <a
+                  href="#"
+                  className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                >
+                  读码
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="relative group">
+            <button className="py-8 group-hover:text-red-500">影视</button>
+            <div className="absolute hidden group-hover:block z-10 w-32 -right-10 text-center gap-4">
+              <div className="p-2">
+                <a
+                  className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  href="#"
+                >
+                  随便看看
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="relative group">
+            <button className="py-8 group-hover:text-red-500">更多</button>
+            <div className="absolute hidden group-hover:block z-10 w-32 -right-10 text-center gap-4">
+              <div className="p-2">
+                <a
+                  className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  href="https://blog.csdn.net/u013351484?type=blog"
+                  target="_blank"
+                >
+                  早期博客
+                </a>
+                <a
+                  className="block p-2 bg-gray-200 hover:bg-gray-300 hover:text-gray-800 active:bg-gray-400"
+                  href="#"
+                >
+                  随便写写
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </header>
     </div>
